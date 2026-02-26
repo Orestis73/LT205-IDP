35c0dfdf8e7f2b91418d17059b3b0179e64c9114
Orestis73 <oz236@cam.ac.uk>
2026-02-26 11:17:07 +0000

    Added comments in test file

--- tests/test_motors_pair.py
+++ tests/test_motors_pair.py
@@ -3,10 +3,12 @@ import config
 from hw.motors import DCMotor, MotorPair
 
 def main():
+    #Initialize motors
     left = DCMotor(config.MOTOR_LEFT_DIR_PIN, config.MOTOR_LEFT_PWM_PIN, config.MOTOR_PWM_FREQ_HZ, config.MOTOR_LEFT_INVERT)
     right = DCMotor(config.MOTOR_RIGHT_DIR_PIN, config.MOTOR_RIGHT_PWM_PIN, config.MOTOR_PWM_FREQ_HZ, config.MOTOR_RIGHT_INVERT)
     drive = MotorPair(left, right)
 
+    #Test motor control by running forward, reverse, and turns
     print("Forward")
     drive.set_left_right(0.3, 0.3); sleep(1)
     print("Stop")
